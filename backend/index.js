f493414756aca395bcac6a1987e7b1603977c86d
KATIYAR SONU <119746624+katiyarsonu@users.noreply.github.com>
2024-03-17 11:30:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -64,7 +64,9 @@ const corsOptions = {
 
 
     console.log(`PORT: ${PORT}`);
-    app.listen(5000, () => {
-    connectDB();
-    console.log(`Server is running on port ${PORT}`)
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`)
+});
+
 });
